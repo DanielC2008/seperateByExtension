3d2900b8f246a1f9cecf652850f8fc1168dfe268
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-05-11 19:20:48 -0500

    copies files from one dir to another

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 'use strict'
+
 const express = require('express')
 const PORT = process.env.PORT || 3000
 const app = express()
-const { createReadStream } = require('fs')
+const { rename, readdir } = require('fs')
+const path = require('path')
+const ncp = require('ncp').ncp 
 
 let [,,...rest] = process.argv
-console.log('path', rest)
+let transferFrom = rest[0]
+let transferTo = rest[1]
+let callback = () => console.log('done')
 
-app.listen(PORT, () => console.log(`port listening on: ${PORT}`))
+let readdirComplete = (err, files) => {
+  files.forEach( file => {
+    let fromPath = path.join( transferFrom, file )
+    let toPath = path.join( transferTo, file )
+    ncp(fromPath, toPath, callback)
+  })  
+
+}
+readdir(transferFrom, readdirComplete)
+
+app.listen()
