213e718fa7c8e00c3b67ac982fd1baabfaecc4b5
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-05-11 20:06:03 -0500

    getting extension from file

--- server.js
+++ server.js
@@ -10,13 +10,22 @@ const ncp = require('ncp').ncp
 let [,,...rest] = process.argv
 let transferFrom = rest[0]
 let transferTo = rest[1]
-let callback = () => console.log('done')
+
+let getExt = fullFile => fullFile.slice(fullFile.lastIndexOf(".") + 1 )
+
 
 let readdirComplete = (err, files) => {
   files.forEach( file => {
-    let fromPath = path.join( transferFrom, file )
-    let toPath = path.join( transferTo, file )
-    ncp(fromPath, toPath, callback)
+    //get ext
+    let ext = getExt(file)
+    //check if file is in dir w/ ext name is in toPath  
+    //if dir exists
+      //copy file into this dir
+    // else 
+      // create dir copy file
+    // let fromPath = path.join( transferFrom, file )
+    // let toPath = path.join( transferTo, file )
+    // ncp(fromPath, toPath)
   })  
 
 }
